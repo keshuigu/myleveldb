6c0d9b6a1e64fff69d7ee25ae3749df253279d7e
keshuigu <keshuigu_yl@qq.com>
2024-11-13 15:40:31 +0800

    finish histogram

--- db/leveldbutil.cc
+++ db/leveldbutil.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
 
@@ -6,12 +7,15 @@
 #include "leveldb/status.h"
 #include "util/posix_logger.h"
 int main() {
-  std::FILE* fp_ = fopen("/mnt/share/workspace/myleveldb/testlog", "w");
-  leveldb::Logger* info_log = new leveldb::PosixLogger(fp_);
-  leveldb::Log(info_log,"hello_log %s","leveldb log test");
-  leveldb::Status s = leveldb::Status::IOError(leveldb::Slice("hello"),
-                                               leveldb::Slice("Status"));
-  std::cout << s.ToString() << std::endl;
-  fclose(fp_);
+  double a[5] = {1, 2, 3, 4, 5};
+  double* x = std::upper_bound(std::begin(a), std::end(a), 3);
+  std::cout << x - std::begin(a) << std::endl;
+  // std::FILE* fp_ = fopen("/mnt/share/workspace/myleveldb/testlog", "w");
+  // leveldb::Logger* info_log = new leveldb::PosixLogger(fp_);
+  // leveldb::Log(info_log,"hello_log %s","leveldb log test");
+  // leveldb::Status s = leveldb::Status::IOError(leveldb::Slice("hello"),
+  //                                              leveldb::Slice("Status"));
+  // std::cout << s.ToString() << std::endl;
+  // fclose(fp_);
   return 0;
 }
